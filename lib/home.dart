d52ed3fc4f454da595419b72fbb281a6baa50fb2
whaleseai7 <whaleseai7@gmail.com>
2023-05-23 22:04:25 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Halaman 2")),
+        body: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text("Kembali"),
+        ));
+  }
+}
